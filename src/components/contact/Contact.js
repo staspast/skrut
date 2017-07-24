c25a6a453cabbb2f4e4db8acedf493fd44f69cdf
staspast <staspast1@gmail.com>
2017-07-24 17:06:37 +0200

    Added contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { FormGroup, ControlLabel, FormControl } from  'react-bootstrap';
 
-const Contact = () =>
-    <form>
+class Contact extends Component{
+
+  constructor (){
+    super()
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      nameValid: false,
+      emailValid: false,
+      messageValid: false
+    }
+  }
+
+  validateNotEmpty = (value) => {
+    return value !== '' || value !== undefined;
+  }
+
+  validateEmail = (email) => {
+  }
+
+  handleNameChange = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({email: event.target.value});
+  }
+
+  handleMessageChange = (event) => {
+    this.setState({message: event.target.value});
+  }
+
+  render(){
+    return(
+      <form>
         <FormGroup
-            controlId="formBasicText"
+          controlId="nameText"
         >
-            <ControlLabel>Working example with validation</ControlLabel>
-            <FormControl
-                type="text"
-                placeholder="Enter text"
-            />
-            <FormControl.Feedback />
+          <ControlLabel>Name</ControlLabel>
+          <FormControl
+            type="text"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
         </FormGroup>
-    </form>;
+
+        <FormGroup
+          controlId="emailText"
+        >
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            type="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+        </FormGroup>
+
+        <FormGroup
+          controlId="messageText"
+        >
+          <ControlLabel>Message</ControlLabel>
+          <FormControl
+            componentClass="textarea"
+            placeholder="Message"
+            value={this.state.message}
+            onChange={this.handleMessageChange}
+          />
+        </FormGroup>
+      </form>
+    )
+  }
+}
 
 export default Contact;
