e982f96bf18461aa886eae229be96aa63f9c206d
andriyspas <spas-andriy2008@yandex.ru>
2017-07-29 11:45:28 +0200

    changed type tabs in offers page

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col, Tab, Nav, NavItem } from  'react-bootstrap';
+import { Grid, Row, Col, Tab, Tabs } from  'react-bootstrap';
 import Header from  './Header';
 import LightboxGallery from '../containers/LightboxGallery';
 
@@ -8,71 +8,54 @@ const Offers = () =>
         <Header/>
 
         <Grid>
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <Row className="clearfix">
-                    <Col sm={12}>
-                        <Nav bsStyle="pills">
-                            <NavItem eventKey="first">
-                                Tab 1
-                            </NavItem>
-                            <NavItem eventKey="second">
-                                Tab 2
-                            </NavItem>
-                        </Nav>
-                    </Col>
-                    <Col sm={12}>
-                        <Tab.Content animation>
-                            <Tab.Pane eventKey="first">
-                                <Row>
-                                    <Col md={7}>
-                                        <div className="offers-title">ПЕРШИЙ НОМЕР</div>
+            <Tabs defaultActiveKey={1}>
+                <Tab eventKey={1} title="Перший номер">
+                    <Row>
+                        <Col md={5}>
+                            <div className="offers-title">Перший номер</div>
 
-                                        <div>Інформація про номер:</div>
-                                        <div>Вітальня, 16 кв. м.</div>
-                                        <div>Спальня 15 кв.</div>
-                                        <div>Санвузол 9 м.кв.</div>
-                                        <div>Вміст:</div>
-                                        <div>Розкладна канапа "американка" 1,60х2,00</div>
-                                        <div>Холодильник</div>
-                                        <div>Телевізор (SAt TV Hotbirt, Sirius, Amos)</div>
-                                        <div>шафа для речей</div>
-                                        <div>Велике дерев`яне ліжко 2.00x1.80</div>
-                                        <div>фен</div>
-                                    </Col>
+                            <div>Інформація про номер:</div>
+                            <div>Вітальня, 16 кв. м.</div>
+                            <div>Спальня 15 кв.</div>
+                            <div>Санвузол 9 м.кв.</div>
+                            <div>Вміст:</div>
+                            <div>Розкладна канапа "американка" 1,60х2,00</div>
+                            <div>Холодильник</div>
+                            <div>Телевізор (SAt TV Hotbirt, Sirius, Amos)</div>
+                            <div>шафа для речей</div>
+                            <div>Велике дерев`яне ліжко 2.00x1.80</div>
+                            <div>фен</div>
+                        </Col>
 
-                                    <Col md={5}>
-                                        <LightboxGallery/>
-                                    </Col>
-                                </Row>
-                            </Tab.Pane>
+                        <Col md={7}>
+                            <LightboxGallery/>
+                        </Col>
+                    </Row>
+                </Tab>
+                <Tab eventKey={2} title="Другий номер">
+                    <Row>
+                        <Col md={5}>
+                            <div>ДРУГИЙ НОМЕР</div>
 
-                            <Tab.Pane eventKey="second">
-                                <Row>
-                                    <Col md={7}>
-                                        <div>ДРУГИЙ НОМЕР</div>
+                            <div>Інформація про номер:</div>
+                            <div>Вітальня, 15 кв. м.</div>
+                            <div>Спальня 15 кв.</div>
+                            <div>Санвузол 5 м.кв.</div>
+                            <div>Вміст:</div>
+                            <div>Розкладна канапа "американка" 1,60х2,00</div>
+                            <div>Холодильник</div>
+                            <div>Телевізор (SAt TV Hotbirt, Sirius, Amos)</div>
+                            <div>Шафа для речей та одягу</div>
+                            <div>Дерев`яне ліжко 2.00x1.40, Ортопедичний матрас</div>
+                            <div>фен</div>
+                        </Col>
 
-                                        <div>Інформація про номер:</div>
-                                        <div>Вітальня, 15 кв. м.</div>
-                                        <div>Спальня 15 кв.</div>
-                                        <div>Санвузол 5 м.кв.</div>
-                                        <div>Вміст:</div>
-                                        <div>Розкладна канапа "американка" 1,60х2,00</div>
-                                        <div>Холодильник</div>
-                                        <div>Телевізор (SAt TV Hotbirt, Sirius, Amos)</div>
-                                        <div>Шафа для речей та одягу</div>
-                                        <div>Дерев`яне ліжко 2.00x1.40, Ортопедичний матрас</div>
-                                        <div>фен</div>
-                                    </Col>
-
-                                    <Col md={5}>
-                                        <LightboxGallery/>
-                                    </Col>
-                                </Row>
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Col>
-                </Row>
-            </Tab.Container>
+                        <Col md={7}>
+                            <LightboxGallery/>
+                        </Col>
+                    </Row>
+                </Tab>
+            </Tabs>
         </Grid>
     </div>;
 
--- src/containers/LightboxGallery.js
+++ src/containers/LightboxGallery.js
@@ -42,6 +42,20 @@ class LightboxGallery extends Component {
                     w: 1200,
                     h: 900,
                     title: 'Image 4'
+                },
+                {
+                    src: 'http://lorempixel.com/1200/900/nightlife/2',
+                    thumbnail: 'http://lorempixel.com/120/90/nightlife/2',
+                    w: 1200,
+                    h: 900,
+                    title: 'Image 2'
+                },
+                {
+                    src: 'http://lorempixel.com/1200/900/nightlife/3',
+                    thumbnail: 'http://lorempixel.com/120/90/nightlife/3',
+                    w: 1200,
+                    h: 900,
+                    title: 'Image 3'
                 }
             ],
             options: {}
