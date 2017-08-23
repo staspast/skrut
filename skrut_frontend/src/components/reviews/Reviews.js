10419d14c56fe269c582b244abcc003621333455
andriyspas <spas-andriy2008@yandex.ru>
2017-08-23 20:52:40 +0200

    clear

--- skrut_frontend/src/components/reviews/Reviews.js
+++ skrut_frontend/src/components/reviews/Reviews.js
@@ -51,56 +51,6 @@ class Reviews extends Component {
                                 </div>
                             </div>
                         </Col>
-
-                        <Col xs={12}>
-                            <div className="reviews__page">
-                                <div className="reviews__data">
-                                    <div className="reviews__data--name">Andriy Spas</div>
-                                    <div className="reviews__data--city">Lviv</div>
-                                    <div className="reviews__data--description">
-                                        Lorem Ipsum is simply text of the printing and typesetting industry is simply text
-                                        of
-                                        the printing and typesetting industry. Lorem Ipsum is simply text of the printing
-                                        and typesetting
-                                        industry is simply text of the printing and typesetting industry. Lorem Ipsum is
-                                        simply text of the
-                                        printing and typesetting industry is simply text of
-                                        the printing and typesetting industry.
-                                    </div>
-                                </div>
-
-                                <div className="reviews__date">
-                                    <div className="reviews__date--day">12</div>
-                                    <div className="reviews__date--month">July</div>
-                                    <div className="reviews__date--year">2017</div>
-                                </div>
-                            </div>
-                        </Col>
-
-                        <Col xs={12}>
-                            <div className="reviews__page">
-                                <div className="reviews__data">
-                                    <div className="reviews__data--name">Andriy Spas</div>
-                                    <div className="reviews__data--city">Lviv</div>
-                                    <div className="reviews__data--description">
-                                        Lorem Ipsum is simply text of the printing and typesetting industry is simply text
-                                        of
-                                        the printing and typesetting industry. Lorem Ipsum is simply text of the printing
-                                        and typesetting
-                                        industry is simply text of the printing and typesetting industry. Lorem Ipsum is
-                                        simply text of the
-                                        printing and typesetting industry is simply text of
-                                        the printing and typesetting industry.
-                                    </div>
-                                </div>
-
-                                <div className="reviews__date">
-                                    <div className="reviews__date--day">12</div>
-                                    <div className="reviews__date--month">July</div>
-                                    <div className="reviews__date--year">2017</div>
-                                </div>
-                            </div>
-                        </Col>
                     </Row>
                 </Grid>
             </div>
